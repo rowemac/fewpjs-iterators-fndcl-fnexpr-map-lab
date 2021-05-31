d602afba446d9af8e53affb9e00adb8e72f17e5e
Mac Rowe <m.rowe42@gmail.com>
2021-05-31 09:32:28 -0400

    commit tree a6d223e28a95a0e53fdbbb7cc80419302314cf03

--- index.js
+++ index.js
@@ -11,6 +11,11 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+const titleCased = (array) => {
+  return tutorials.map( string => {
+    let words = string.split(' ')
+    let capWords = words.map( word => word.charAt(0).toUpperCase() + word.slice(1))
+    let result = capWords.join(' ')
+    return result
+  })
+};
